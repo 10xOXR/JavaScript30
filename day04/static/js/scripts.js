97ad759a03a1e3c2cb26e3dc83459ccf7dadeea4
10xOXR <10xoxr@gmail.com>
2019-11-12 17:35:37 +0000

    Create Array.prototype.filter against 'inventors' array
    Created both long and arrow-function versions of Array.prototype.filter.

--- day04/static/js/scripts.js
+++ day04/static/js/scripts.js
@@ -89,6 +89,15 @@ const people = [
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
 
+// const fifteenCent = inventors.filter((inventor) => {
+//     if (inventor.year >= 1500 && inventor.year < 1600) {
+//         return true;
+//     } 
+// });
+
+const fifteenCent = inventors.filter((inventor) => inventor.year >= 1500 && inventor.year < 1600);
+
+console.table(fifteenCent);
 
 // Array.prototype.map()
 // 2. Give us an array of the inventors' first and last names
