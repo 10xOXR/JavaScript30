180dfbe14817fc8c5ea8c1d5e8eab9d67e1fb659
10xOXR <10xoxr@gmail.com>
2019-12-01 11:20:27 +0000

    Complete JS functionality

--- day17/static/js/scripts.js
+++ day17/static/js/scripts.js
@@ -4,3 +4,13 @@ const bands = [
     'We Came as Romans', 'Counterparts', 'Oh, Sleeper',
     'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'
 ];
+
+function remArticles(bandName) {
+    return bandName.replace(/^(the |a |an )/i, "").trim();
+}
+
+const sortedNames = bands.sort((a, b) => remArticles(a) > remArticles(b) ? 1 : -1);
+
+document.querySelector("#bands").innerHTML = sortedNames.map(band => `<li>${band}</li>`).join("");
+
+console.log(sortedNames);
