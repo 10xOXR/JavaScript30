6e1acc67eef3da878ec78ced2dda21d089ee0cb1
10xOXR <10xoxr@gmail.com>
2019-12-22 09:49:23 +0000

    Add fixed navbar functionality

--- day24/static/js/scripts.js
+++ day24/static/js/scripts.js
@@ -1,8 +1,14 @@
 const nav = document.querySelector("#main");
+const topNav = nav.offsetTop;
 
-function fixnav() {
-    console.log("Fired");
-
+function fixNav() {
+    if (window.scrollY >= topNav) {
+        document.body.classList.add("fixed-nav");
+        document.body.style.paddingTop = nav.offsetHeight + "px";
+    } else {
+        document.body.classList.remove("fixed-nav");
+        document.body.style.paddingTop = 0;
+    }
 }
 
-window.addEventListener("scroll", fixnav);
+window.addEventListener("scroll", fixNav);
