a18711d5797eec77bdd3be11b7cbd2b68f760c65
10xOXR <10xoxr@gmail.com>
2020-01-11 13:59:25 +0000

    Create peep and startGame functions

--- day30/static/js/scripts.js
+++ day30/static/js/scripts.js
@@ -2,9 +2,11 @@ const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
 let lastHole;
+let timeUp = false;
+let score = 0;
 
-function randTime(min, max) {
-    return Math.round(Math.random(max - min) + min);
+function randomTime(min, max) {
+    return Math.round(Math.random() * (max-min) + min);
 }
 
 function randomHole(holes) {
@@ -15,4 +17,24 @@ function randomHole(holes) {
     }
     lastHole = hole;
     return hole;
+}
+
+function peep() {
+    const time = randomTime(200, 1000);
+    const hole = randomHole(holes);
+
+    hole.classList.add("up");
+    setTimeout(() => {
+        hole.classList.remove("up");
+        if (!timeUp) peep();
+    }, time);
+}
+
+function startGame() {
+    scoreBoard.textContent = 0;
+    timeUp = false;
+    peep();
+    setTimeout(() => {
+        timeUp = true;
+    }, 10000);
 }
